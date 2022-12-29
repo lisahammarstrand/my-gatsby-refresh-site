8eef51d2a1e937e6f3c585783968f2ceeb739198
Your Name <you@example.com>
2022-12-29 10:23:27 +0100

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `My Gatsby refresh site`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
